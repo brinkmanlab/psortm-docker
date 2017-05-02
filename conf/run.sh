ad4a3503d5d0c74de51d68e164f5ebe78caa2679
gemmahoad <ghoad@sfu.ca>
2017-05-02 16:11:33 -0700

    This is the entrypoint file which restarts docker on system restart. Permission change commands previously in the dockerfile have been added to this script because they weren't working there (conflict occurring when mounting directories?).

--- conf/run.sh
+++ conf/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# permission and environment setting on container start/restart
+chmod 777 /tmp/psortm
+chmod -R 777 /var/www/html/taxon_predictor
+echo $MOUNT_DIRECTORY > /var/www/html/mounted_dir.conf
+
 # Start up supervisor
 /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
