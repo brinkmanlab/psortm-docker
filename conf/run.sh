89ae9ae33b778dd92b1b2c02648cec8ee47a1d5e
gemmahoad <ghoad@sfu.ca>
2017-05-18 10:00:12 -0700

    Added code for making sure correct version of apache2 service file is in place and executable.

--- conf/run.sh
+++ conf/run.sh
@@ -3,6 +3,7 @@
 # permission and environment setting on container start/restart
 chmod 777 /tmp/psortm
 chmod -R 777 /var/www/html/taxon_predictor
+sed -e "s/20/60/g" < /etc/init.d/apache2.orig > /etc/init.d/apache2 && chmod 755 /etc/init.d/apache2
 echo $MOUNT_DIRECTORY > /var/www/html/mounted_dir.conf
 
 # Start up supervisor
