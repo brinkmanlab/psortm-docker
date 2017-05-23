6e8f570378d7d007abbdd57327a16712b6b8f666
gemmahoad <ghoad@sfu.ca>
2017-05-23 13:22:27 -0700

    Added more 77 permissions throughout to avoid issues

--- conf/run.sh
+++ conf/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # permission and environment setting on container start/restart
-chmod 777 /tmp/psortm
-chmod -R 777 /var/www/html/taxon_predictor
+chmod -R 777 /tmp
+chmod -R 777 /var/www/html
 sed -e "s/20/60/g" < /etc/init.d/apache2.orig > /etc/init.d/apache2 && chmod 755 /etc/init.d/apache2
 echo $MOUNT_DIRECTORY > /var/www/html/mounted_dir.conf
 
